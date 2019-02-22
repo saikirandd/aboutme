4438d1735701f02a2a4c2d3b45f252e0018a5e04
saikirandd <33051183+saikirandd@users.noreply.github.com>
2019-02-22 13:07:58 -0600

    new

--- function.js
+++ function.js
@@ -15,3 +15,19 @@ function LengthConverter(valNum) {
     document.getElementById('result').innerHTML = ans
   })
   console.log('SCRIPT END')  
+  var request = new XMLHttpRequest();
+request.open('GET', 'https://codepen.io/saikirandd/pen/PVrgNL.html');
+request.onreadystatechange = function () { 
+    // 4. check if the request has a readyState of 4, which indicates the server has responded (complete)
+    if (request.readyState === 4) {
+        // 5. insert the text sent by the server into the HTML of the 'ajax-content'
+        document.getElementById('ajax').innerHTML = request.responseText;
+    }
+};
+
+function sendTheAJAX() {
+    request.send();
+    document.getElementById('show').style.display = 'none';
+}
+
+//referred : https://www.w3schools.com/jquery/ajax_ajax.asp
