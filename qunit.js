6ebc05f743a17f37ea27f01d70f0f0745d5890a5
saikirandd <33051183+saikirandd@users.noreply.github.com>
2019-02-22 14:40:07 -0600

    n

--- qunit.js
+++ qunit.js
@@ -9,12 +9,12 @@ function lengthConverterTest(valNum) {
 }
     
 }   
-  
+ src="https://code.jquery.com/qunit/qunit-2.9.2.js"
 //QUnit Testing 5 times
   QUnit.test("LengthConverter", function(assert) {
-	assert.equal(LengthConverter(4, 0.0001016002032004064, "4 inches  == 0.0001016002032004064  kilometers");
-	assert.equal(LengthConverter(10000,0.254000508001016, "10000 inches  == 0.254000508001016 kilometers");
-	assert.equal(LengthConverter(-50, 0, "-50 inches  == 0 kilometers");
-	assert.equal(LengthConverter(748974995, 19024.002921005842, "748974995 inches  == 19024.002921005842 kilometers");
-	assert.equal(LengthConverter(1, 0.0000254000508001016, "1 inches  == 0.0000254000508001016 kilometers");
+	assert.equal(LengthConverter(4, 0.0001016002032004064, "4 inches  == 0.0001016002032004064  kilometers"));
+	assert.equal(LengthConverter(10000,0.254000508001016, "10000 inches  == 0.254000508001016 kilometers"));
+	assert.equal(LengthConverter(-50, 0, "-50 inches  == 0 kilometers"));
+	assert.equal(LengthConverter(748974995, 19024.002921005842, "748974995 inches  == 19024.002921005842 kilometers"));
+	assert.equal(LengthConverter(1, 0.0000254000508001016, "1 inches  == 0.0000254000508001016 kilometers"));
   });
